e211ee0c28b2735f6f90b4cee54fd7e1eb1ab7f8
janbermet <81546071+janbermet@users.noreply.github.com>
2022-04-05 11:40:26 +0600

    Update index.php

--- index.php
+++ index.php
@@ -471,7 +471,7 @@
             <p> <i class="fas fa-envelope"></i> fitness@gmail.com </p>
             <p> <i class="fas fa-map"></i> Kyrgyzstan, Talas  </p>
             <div class="share">
-                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="https://www.facebook.com/Meta/" class="fab fa-facebook-f"></a>
                 <a href="#" class="fab fa-twitter"></a>
                 <a href="#" class="fab fa-linkedin"></a>
                 <a href="#" class="fab fa-pinterest"></a>
